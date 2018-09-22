721ebe8425e9720d6ae45ec89b63d3c2816ac7cc
Paulo Henrique <paulo.441997@hotmail.com>
2018-09-22 19:59:32 -0300

    recuperar senha

--- index.php
+++ index.php
@@ -184,8 +184,44 @@ $app->get("/admin/forgot/sent", function(){
 
 $app->get("/admin/forgot/reset", function(){
 
+	$user = User::validForgotDecrypt($_GET["code"]);
+
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+
+	$page->setTpl("forgot-reset", array(
+		"name"=>$user["desperson"],
+		"code"=>$_GET["code"]
+	));
 	
-	
+});
+
+$app->post("/admin/forgot/reset", function(){
+
+	$forgot = User::validForgotDecrypt($_POST["code"]);
+
+	User::setForgotUsed($forgot["idrecovery"]);
+
+	$user = new User();
+
+	$password = password_hash($_POST["password"], PASSWORD_DEFAULT, [
+			"cost"=>12
+	]);
+
+	$user->get((int)$forgot["iduser"]);
+
+	$user->setPassword($password);
+
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+
+	$page->setTpl("forgot-reset-success");
+
+
 });
 
 $app->run();
