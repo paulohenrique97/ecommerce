eae723015fcc8053ca22b27aa7b3efc6be620bf1
Paulo Henrique <paulo.441997@hotmail.com>
2018-09-23 15:53:05 -0300

    Site - Categorias

--- vendor/hcodebr/php-classes/src/Model/Category.php
+++ vendor/hcodebr/php-classes/src/Model/Category.php
@@ -32,6 +32,8 @@ class Category extends Model {
 
         $this->setData($results[0]);
 
+        Category::updateFile();
+
     }
 
     public function get($idcategory){
@@ -57,6 +59,24 @@ class Category extends Model {
             idcategory = :idcategory", [
                 ":idcategory"=>$this->getidcategory()
             ]);
+
+        Category::updateFile();
+
+    }
+
+    public static function updateFile(){
+
+        $categories = Category::listAll();
+
+        $html = [];
+
+        foreach ($categories as $row) {
+            
+            array_push($html, '<li><a href="/categories/'.$row['idcategory'].'">'.$row['descategory'].'</a></li>');
+
+        }
+
+        file_put_contents($_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . "views" . DIRECTORY_SEPARATOR . "categories-menu.html", implode('', $html));
 
     }
 
