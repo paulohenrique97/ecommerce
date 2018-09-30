17e7048c43c7cbe636dd13ea3c6e10caa418829e
Paulo Henrique <paulo.441997@hotmail.com>
2018-09-30 16:28:16 -0300

    Site cadastro

--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -296,6 +296,40 @@ class User extends Model {
 
     }
 
+    public static function setErrorRegister($msg){
+
+        $_SESSION[User::ERROR_REGISTER] = $msg;
+
+    }
+
+    public static function getErrorRegister(){
+
+        $msg = (isset($_SESSION[User::ERROR_REGISTER]) && $_SESSION[User::ERROR_REGISTER]) ? $_SESSION[User::ERROR_REGISTER] : '';
+
+        User::clearErrorRegister();
+
+        return $msg;
+
+    }
+
+    public static function clearErrorRegister(){
+
+        $_SESSION[User::ERROR_REGISTER] = NULL;
+
+    }
+
+    public static function checkLoginExist($login){
+
+        $sql = new Sql();
+
+        $results = $sql->select("SELECT * FROM tb_users WHERE deslogin = :deslogin", [
+            ':deslogin'=>$login
+            ]);
+
+        return(count($results) > 0);
+
+    }
+
 }
 
 ?>
