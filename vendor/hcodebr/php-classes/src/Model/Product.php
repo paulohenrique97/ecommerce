8f03ea659561f23ea0be9f6547d88f250419abfd
Paulo Henrique <paulo.441997@hotmail.com>
2018-10-02 15:15:38 -0300

    Admin - Pedidos - Busca e Paginação

--- vendor/hcodebr/php-classes/src/Model/Product.php
+++ vendor/hcodebr/php-classes/src/Model/Product.php
@@ -176,7 +176,7 @@ class Product extends Model {
 
     }
 
-     public static function getPage($page = 1, $itemsPerPage = 10){
+    public static function getPage($page = 1, $itemsPerPage = 10){
 
         $start = ($page-1) * $itemsPerPage;
 
